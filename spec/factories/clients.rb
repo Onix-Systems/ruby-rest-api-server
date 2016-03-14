cab4d3cfbd3095e8f0c979c3237f9df7be28bbe2
Victor Nagorny <victor.nagorny@onix-systems.com>
2016-03-14 21:01:47 +0200

    Add factories for client and product modelds

---
+++ spec/factories/clients.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :client do
+    client_type 0
+    gln 4_820_001_350_005
+    full_name "Private Joint Stock Company 'Chumak'"
+    short_name "PJSC 'Chumak'"
+  end
+end
