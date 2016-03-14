dd10f07413e5045d17d8b60499b0be0d4ab49663
Victor Nagorny <victor.nagorny@onix-systems.com>
2016-03-14 11:01:12 +0200

    Update client and product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,12 @@
 class Product < ActiveRecord::Base
-  validates :gtin, :information_provider_gln, :bar_code_type, :unit_descriptor,
-            :internal_supplier_code, :brand_name, :description_short, :description_full, presence: true
+  validates :gtin, :bar_code_type, :unit_descriptor, :internal_supplier_code,
+            :brand_name, :description_short, :description_full, presence: true
 
-  validates :gtin, :information_provider_gln, numericality: { only_integer: true }
-
-  validates :description_short, length: { minimum: 4, maximum: 140 }
-
-  validates :description_full, length: { minimum: 5, maximum: 500 }
+  validates :gtin, numericality: { only_integer: true }
 
   validates :gtin, uniqueness: true
+
+  validates :description_short, length: { maximum: 140 }
+
+  validates :description_full, length: { maximum: 500 }
 end
