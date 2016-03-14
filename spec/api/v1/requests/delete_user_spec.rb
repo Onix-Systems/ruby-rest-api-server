453de3e5fe36d3f76a5f7945ee6a34a25f526882
Victor Nagorny <victor.nagorny@onix-systems.com>
2016-03-14 20:21:31 +0200

    Add /api/v1/ to context description of specs

--- spec/api/v1/requests/delete_user_spec.rb
+++ spec/api/v1/requests/delete_user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'API Account Deletion', type: :request do
   include_context 'configure Warden test helpers'
   include_context 'sign in user and create new auth token'
 
-  context 'DELETE /auth' do
+  context 'DELETE /api/v1/auth' do
     it 'destroys users identified by their uid and auth_token headers' do
       delete api_v1_user_registration_path, {}, headers
 
