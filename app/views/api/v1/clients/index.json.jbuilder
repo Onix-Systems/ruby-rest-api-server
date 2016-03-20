830487f553c9217335408492aa95599e97bc9f40
Victor Nagorny <victor.nagorny@onix-systems.com>
2016-03-20 14:29:18 +0200

    Add API display a list of all clients spec

--- app/views/api/v1/clients/index.json.jbuilder
+++ app/views/api/v1/clients/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@clients) do |client|
   json.extract! client, :id, :client_type, :gln, :full_name, :short_name, :description
-  json.url client_url(client, format: :json)
+  json.url api_v1_client_url(client, format: :json)
 end
---
+++ spec/api/v1/requests/client/display_a_list_of_all_clients_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'API Update Client', type: :request do
+  let!(:email) { Faker::Internet.email }
+  let!(:password) { Faker::Internet.password }
+
+  let!(:user) { create(:user, email: email, password: password) }
+  let!(:headers) { { 'ACCEPT' => 'application/json' } }
+
+  include_context 'configure Warden test helpers'
+  include_context 'sign in user and create new auth token'
+
+  before do
+    create(:client)
+  end
+
+  context 'GET /api/v1/clients' do
+    it 'display a list of all clients' do
+      client_attributes = attributes_for(:client)
+
+      get api_v1_clients_path, {}, headers
+
+      expect(response).to have_http_status(200)
+      expect(response.content_type).to eq('application/json')
+
+      expect(json.length).to eq(1)
+      expect(json[0][:client_type]).to eq(client_attributes[:client_type])
+      expect(json[0][:gln]).to eq(client_attributes[:gln])
+      expect(json[0][:full_name]).to eq(client_attributes[:full_name])
+      expect(json[0][:short_name]).to eq(client_attributes[:short_name])
+    end
+  end
+end
